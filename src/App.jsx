74207483ba02f62442f63ec2022d62aa614b3e88
ahmadadibzad <a.adibzad@gmail.com>
2023-12-05 23:40:23 +0330

    Add signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import LoginPage from './components/Authentication/LoginPage'
+import SignupPage from './components/Authentication/SignupPage'
 import CartPage from './components/Cart/CartPage'
 import HomePage from './components/Home/HomePage'
 import MyOrderPage from './components/MyOrder/MyOrderPage'
@@ -18,6 +19,7 @@ const App = () => {
         <CartPage />
         <MyOrderPage />
         <LoginPage />
+        <SignupPage />
       </main>
     </div>
   )
